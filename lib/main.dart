7c903b7355c01b7587e2ec30ae630d19d5ea563c
Robert Ngo <robert.ngo@evolvingweb.ca>
2019-12-27 18:40:49 -0500

    Create scaffold app with StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(NYTBooksApp());
 
+// 1. Create the NYTBooksApp class by extending StatefulWidget, 
+// we then have to override method createState() by returning 
+// an instance of class NYTBooksState()
 class NYTBooksApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -9,13 +12,19 @@ class NYTBooksApp extends StatefulWidget {
   }
 }
 
+// 2. Class NYTBooksState()
 class NYTBooksState extends State<NYTBooksApp> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      // 2.1 Customize app theme by changing primaryColor
       theme: new ThemeData(
         primaryColor: Colors.white,
       ),
+
+      // 2.2 Build the home screen, with:
+      // - appBar on top showing the app name, and a refresh button
+      // - body with a CircularProcessIndicator() in the center
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text("NY Times books"),
