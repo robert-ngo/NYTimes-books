d0fef9d22d5bf56ea24052bcc792885caeb28517
Robert Ngo <robert.ngo@evolvingweb.ca>
2019-12-28 19:06:01 -0500

    Render book items as listView

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,43 @@ class NYTBooksState extends State<NYTBooksApp> {
           ],
         ),
         body: new Center(
-          child: _isLoading ? new CircularProgressIndicator() : new Text("Finished fetching data!"),
+          child: _isLoading ? new CircularProgressIndicator() : 
+            new ListView.builder(
+              itemCount: this.books != null ? this.books.length : 0,
+              itemBuilder: (context, i) {
+                final book = this.books[i];
+
+                final imageUrl = book["book_image"];
+                final publisher = book["publisher"];
+                final weeksOnList = book["weeks_on_list"];
+                final author = book["author"];
+
+                return new Container(
+                  padding: new EdgeInsets.all(16.0),
+                  child: new Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Image.network(imageUrl, width: 100.0,),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.only(left: 16.0),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("$weeksOnList weeks on the list".toUpperCase(), style: TextStyle(fontSize: 12.0),),
+                              Text(book["title"], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),),  
+                              Text("by $author | $publisher",),
+                              new Container(height: 8.0,),
+                              Text(book["description"], style: TextStyle(fontFamily: 'georgia'),)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ]
+                  )
+                );
+              },
+            )
         ),
       ),
     );
